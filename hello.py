6ef37dfe4e2f54e52e99858fddff46f21e530e81
Morgis, Matt (ELS-PHL) <m.morgis@elsevier.com>
2017-07-21 20:52:03 -0400

    Initial commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+class MyClass:
+
+    def __init__(self):
+        return
+
+    def function_to_test(self, x, y):
+        return x + y
---
+++ hello_test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MetamapAPITest(unittest.TestCase):
+
+    def test_one(self):
+        assertEqual(true, false)
+
+
+if __name__ == '__main__':
+    unittest.main()
